89dad9f1954605fa58ab3bee8ca122220f31ff04
smitdonda <94434466+smitdonda@users.noreply.github.com>
2021-12-26 14:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// fetch api and print data in html page 
+function getdata(){
+    // URL
+    url = "https://bobsburgers-api.herokuapp.com/characters"
+    fetch(url)
+        .then((response)=>response.json())
+        .then((data)=>{
+        let output ="";
+        // debugger;
+        data.forEach(function(user){
+            output +=`
+            // table rows and table data 
+            <tr>
+                <td scope="col">${user.id}</td>
+                <td scope="col">${user.name}</td> 
+                <td scope="col">${user.gender}</td>
+                <td scope="col">${user.hairColor}</td>
+                <td scope="col">${user.occupation}</td>
+            </tr>    
+            `;
+        });
+        //print the table data
+        document.getElementById('user').innerHTML = output;
+        //any error print the error in console
+    }).catch((error)=>console.log(error))
+}
+getdata()
